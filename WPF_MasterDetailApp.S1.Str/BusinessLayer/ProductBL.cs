36be759933dd75b85efe50e994e39ef2158c806b
nandress09 <andres23@mail.nmc.edu>
2019-02-24 21:25:31 -0500

    Add project files.

---
+++ WPF_MasterDetailApp.S1.Str/BusinessLayer/ProductBL.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WPF_MasterDetailApp;
+using WPF_MasterDetailApp.Models;
+
+namespace WPF_MasterDetailApp.BusinessLayer
+{
+    public class ProductBL
+    {
+        
+
+        #region ENUMS
+
+
+
+        #endregion
+
+        #region FIELDS
+        ProductWindowView _productWindowView;
+        ProductWindowPresenter _productWindowPresenter;
+
+
+        #endregion
+
+        #region PROPERTIES
+
+        #endregion
+
+        #region CONSTRUCTORS
+        public ProductBL()
+        {
+            _productWindowPresenter = new ProductWindowPresenter(GetCompanyData(), GetProductData());
+
+            _productWindowView = new ProductWindowView(_productWindowPresenter);
+            _productWindowView.DataContext = _productWindowPresenter;
+            _productWindowView.Show();
+        }
+
+        private Product GetProductData()
+        {
+            return new Product
+            {
+                Id = 1,
+                OwnerName = "Philip",
+                Description = "Freshly built in 2017. One acre of land total. No neibors for miles. Big Basement.",
+                Price = 600000.00,
+                Style = Product.HouseStyle.Modern,
+                Beds = 2,
+                Baths = 2,
+                ListingDateTime = new DateTime(2019, 01, 26),
+                ImageFileName = "house.jpg"
+            };
+        }
+        private Company GetCompanyData ()
+        {
+            return new Company
+            {
+                Name = "Sara Lee Realestate",
+                Address = "9887 Carriage St.",
+                City = "Traverse City"
+            };
+        }
+
+        #endregion
+
+        #region METHODS
+
+
+
+        #endregion
+
+        #region EVENTS
+
+
+        #endregion
+
+    }
+}
---
+++ WPF_MasterDetailApp.S1.Str/Models/Product.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_MasterDetailApp.Models
+{
+    public class Product
+    {
+        #region ENUMS
+
+        public enum HouseStyle { undefined, Modern, Traditional }
+
+        #endregion
+
+        #region FIELDS
+
+        private int _id;
+        private string _ownerName;
+        private string _description;
+        private double _price;
+        private int _baths;
+        private int _beds;
+        private DateTime _listingDateTime;
+        private HouseStyle _style;
+        private string _imageFileName;
+
+        
+        #endregion
+
+        #region PROPERTIES
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string OwnerName
+        {
+            get { return _ownerName; }
+            set { _ownerName = value; }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        public double Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+
+        public int Baths
+        {
+            get { return _baths; }
+            set { _baths = value; }
+        }
+
+        public int Beds
+        {
+            get { return _beds; }
+            set { _beds = value; }
+        }
+
+        public DateTime ListingDateTime
+        {
+            get { return _listingDateTime; }
+            set { _listingDateTime = value; }
+        }
+
+        public HouseStyle Style
+        {
+            get { return _style; }
+            set { _style = value; }
+        }
+
+        public string ImageFileName
+        {
+            get { return _imageFileName; }
+            set { _imageFileName = value; }
+        }
+
+        public string ImageFilePath
+        {
+            get { return @"../Images/" + _imageFileName; }
+        }
+
+        #endregion
+
+        #region CONSTRUCTORS
+
+        //sprint two stuff public Product()
+        // {
+
+        // }
+
+        #endregion
+
+        #region METHODS
+
+
+        #endregion
+
+        #region EVENTS
+
+
+
+        #endregion
+    }
+}
